d64d6806db1b67385047b6d4343fcb9aacb63e2d
Seacolorfox <Seacolorfox@outlook.com>
2019-07-13 16:32:21 +0800

    some change

--- WindowsFormsAppTest/WindowsFormsAppTest/Form2.Designer.cs
+++ WindowsFormsAppTest/WindowsFormsAppTest/Form2.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(528, 355);
+            this.Name = "Form2";
+            this.Text = "WindowsFrom2";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
